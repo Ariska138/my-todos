e4b926989e4bfac71c1863a9349475b0c58992a4
Ariska Hidayat <ariska138@gmail.com>
2025-10-06 08:48:17 +0700

    login

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import { Hono } from 'hono';
 import { serve } from '@hono/node-server';
+import 'dotenv/config';
 import { db } from './db/index.js';
 import { users } from './db/schema.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { setCookie } from 'hono/cookie';
 
 const app = new Hono();
 
 app.get('/', (c) => c.html('<h1>Tim Pengembang</h1><h2>Nama Kalian</h2>'));
 
-app.post('/register', async (c) => {
+app.post('/api/register', async (c) => {
   try {
     const { username, password } = await c.req.json();
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -21,6 +24,40 @@ app.post('/register', async (c) => {
   } catch (error) {
     return c.json({ success: false, message: 'Registrasi gagal' }, 400);
   }
+});
+
+app.post('/api/login', async (c) => {
+  const { username, password } = await c.req.json();
+  const user = await db.query.users.findFirst({
+    where: (users, { eq }) => eq(users.username, username),
+  });
+
+  if (!user)
+    return c.json(
+      { success: false, message: 'Username atau password salah' },
+      401
+    );
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid)
+    return c.json(
+      { success: false, message: 'Username atau password salah' },
+      401
+    );
+
+  const token = jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.JWT_SECRET,
+    { expiresIn: '1h' }
+  );
+
+  setCookie(c, 'token', token, {
+    httpOnly: true,
+    sameSite: 'Lax',
+    maxAge: 3600,
+  });
+
+  return c.json({ success: true, message: 'Login berhasil' });
 });
 
 serve({ fetch: app.fetch, port: 3000 });
